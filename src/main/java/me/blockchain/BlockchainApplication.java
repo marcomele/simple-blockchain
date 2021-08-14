f6f309f6be557a1afa595946ca493bb361d30853
Marco Mele <melemarco94@gmail.com>
2021-08-14 14:58:23 -0500

    fixes

--- src/main/java/me/blockchain/BlockchainApplication.java
+++ src/main/java/me/blockchain/BlockchainApplication.java
@@ -6,7 +6,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 public class BlockchainApplication {
 
@@ -18,6 +26,13 @@ public class BlockchainApplication {
     public RestTemplate restTemplate() {
         final ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
         return new RestTemplate(requestFactory);
+    }
+
+    @Bean
+    public Docket swagger() {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(new ApiInfoBuilder().title("Simple Blockchain").build())
+                .select().apis(RequestHandlerSelectors.any()).paths(PathSelectors.any())
+                .paths(PathSelectors.ant("/error").negate()).build();
     }
 
 }
